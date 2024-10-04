9ea567b06fd5e13f225d2e63b988106e03bf6e6f
TestNewProj <149118281+TestNewProj@users.noreply.github.com>
2024-10-04 04:15:54 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -68,6 +68,45 @@ sld_cont.forEach((el) => {
   el.addEventListener('mouseup', handleMouseUp);
 })
 
+
+sld_cont.forEach((el) => {
+  el.addEventListener('touchstart', touchDown);
+  el.addEventListener('touchmove', touchUp);
+})
+
+function touchDown(event) {
+  startX = event.touches[0].clientX;
+  startY = event.touches[0].clientY;
+  cl = 1;
+}
+
+function touchUp(event) {
+  if (cl == 1) {
+    const deltaX = event.touches[0].clientX - startX;
+    const deltaY = event.touches[0].clientY - startY;
+    // if (Math.abs(deltaX) > Math.abs(deltaY)
+    if ((deltaX > 10) && (Math.abs(deltaX) > Math.abs(deltaY))) {
+      active = active - 1 >= 0 ? active - 1 : lengthItems;
+
+      slider.style.left = -items[active].offsetLeft + 'px'
+      let last_active_dot = document.querySelector('.slider .dots li.active');
+      last_active_dot.classList.remove('active');
+      dots[active].classList.add('active');
+    }
+    else if ((deltaX < -10) && (Math.abs(deltaX) > Math.abs(deltaY))) {
+      active = active + 1 <= lengthItems ? active + 1 : 0;
+      
+      slider.style.left = -items[active].offsetLeft + 'px'
+      let last_active_dot = document.querySelector('.slider .dots li.active');
+      last_active_dot.classList.remove('active');
+      dots[active].classList.add('active');
+    }
+  }
+  startX = null;
+  startY = null;
+  cl = 0
+}
+
 function handleMouseDown(event) {
   // Get the mouse coordinates
   startX = event.clientX;
@@ -109,3 +148,6 @@ function handleMouseUp(event) {
 
 
 
+Fancybox.bind("[data-fancybox]", {
+  // Your custom options
+});
